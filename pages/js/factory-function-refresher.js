c01203b44a478c93922fdb934b89e068a5317b74
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-12-09 23:25:41 -0300

    Left off on private variables and functions https://www.theodinproject.com/lessons/node-path-javascript-factory-functions-and-the-module-pattern#private-variables-and-functions

---
+++ pages/js/factory-function-refresher.js
@@ -0,0 +1,27 @@
+const name = "Bob";
+const age = 28;
+const color = "red";
+
+const thatObject = {name, age, color};
+
+console.log({...thatObject});
+
+function createUser(name) {
+   const discordName = "@" + name;
+
+   let reputation = 0;
+   const getReputation = () => reputation;
+   const giveReputation = () => reputation++;
+
+   return { name, discordName, getReputation, giveReputation };
+}
+
+const josh = createUser("josh");
+josh.giveReputation();
+josh.giveReputation();
+
+console.log({
+   discordName: josh.discordName,
+   reputation: josh.getReputation()
+});
+// logs { discordName: "@josh", reputation: 2 }
