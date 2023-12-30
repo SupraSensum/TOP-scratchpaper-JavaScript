6fc9d7671813dd5a02664d0397a6ad1125d0a7a8
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-12-30 09:45:09 -0300

    Forgot to commit these

--- pages/src/class-set-get-shits-n-giggles.js
+++ pages/src/class-set-get-shits-n-giggles.js
@@ -1,2 +1,25 @@
 // Let's create a class using everything (or as much as we can recall) we've learned so far, just for shits 'n giggles
 
+// What's this class gonna be about? Well, let's use something we're familiar with: people
+
+// A person has:
+class Person {
+   constructor(name, dob, nationality) {
+      this.name = name;
+      this.dob = dob;
+      this.nationality = nationality;
+   }
+
+   set name(value) {
+      if (value.length < 4) {
+         console.log("Name must be at least 4 characters");
+         return;
+      }
+      
+      this._name = value;
+   }
+
+   get name() {
+      return this._name;
+   }
+}
