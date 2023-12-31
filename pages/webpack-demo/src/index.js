281a3c5ef6083195bc8a906a7a2749af0433ebd9
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-12-31 11:48:34 -0300

    Major file structure overhaul

--- pages/webpack-demo/src/index.js
+++ pages/webpack-demo/src/index.js
@@ -4,6 +4,18 @@ import './style.css';
 import goldblum from './goldblum.png';
 import DataXml from './dataset.xml';
 import DataCsv from './MOCK_DATA.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
    const element = document.createElement('div');
